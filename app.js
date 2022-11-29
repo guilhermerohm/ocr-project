01ef2ce7b9327cc45233d038e5bc04bebc38c532
guilhermerohm <guih_rohm@hotmail.com>
2022-11-28 22:42:37 -0300

    added space in the app.js file

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const worker = await createWorker({
   await worker.loadLanguage('por');
   await worker.initialize('por');
   const { data: { text } } = await worker.recognize('https://lenscope.com.br/blog/wp-content/uploads/2018/11/exemplo-comum.jpg');
+ 
   console.log(text);
+ 
+ 
+ 
   await worker.terminate();
 })();
