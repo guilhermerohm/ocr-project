73393c95f6d15518367ad7dec68a96c8f68da103
guilhermerohm <guih_rohm@hotmail.com>
2022-11-28 22:37:36 -0300

    my first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import { createWorker } from 'tesseract.js';
+
+const worker = await createWorker({
+  logger: m => console.log(m)
+});
+
+(async () => {
+  await worker.loadLanguage('por');
+  await worker.initialize('por');
+  const { data: { text } } = await worker.recognize('https://lenscope.com.br/blog/wp-content/uploads/2018/11/exemplo-comum.jpg');
+  console.log(text);
+  await worker.terminate();
+})();
